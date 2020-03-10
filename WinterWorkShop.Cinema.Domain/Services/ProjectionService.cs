7ccadf4a4a2175a46f130a54785e3c4936c73660
Milorad <durakovicmilorad@gmail.com>
2020-03-10 10:03:28 +0100

    Removing unnecessary from projectwithauditoriumresultmodel

--- WinterWorkShop.Cinema.Domain/Services/ProjectionService.cs
+++ WinterWorkShop.Cinema.Domain/Services/ProjectionService.cs
@@ -308,8 +308,8 @@ namespace WinterWorkShop.Cinema.Domain.Services
 
             ProjectionWithAuditoriumResultModel projModel = new ProjectionWithAuditoriumResultModel
             {
-                IsSuccessful = true,
-                ErrorMessage = null,
+                //IsSuccessful = true,
+                //ErrorMessage = null,
                 Projection = new ProjectionDomainModel
                 {
                     Id = projectionWithAuditorium.Id,
